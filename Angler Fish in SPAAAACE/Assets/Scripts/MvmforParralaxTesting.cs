2ea1c166df1a7cfac14799bccccc67a6ab401d16
zbearwrestler <z.bearinger@gmail.com>
2019-02-16 12:37:54 -0800

    Temp ship moving for paralax

---
+++ Angler Fish in SPAAAACE/Assets/Scripts/MvmforParralaxTesting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MvmforParralaxTesting : MonoBehaviour
+{
+    public float playerspeed;
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float aX = Input.GetAxis("Horizontal");
+        float aY = Input.GetAxis("Vertical");
+
+        Vector3 vel = new Vector3(aX, aY);
+        if (vel.magnitude > 1)
+        {
+            vel.Normalize();
+        }
+
+        rigid.velocity = vel * playerspeed;
+        transform.up = vel;
+ 
+    }
+}
